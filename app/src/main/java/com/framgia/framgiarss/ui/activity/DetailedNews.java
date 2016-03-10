644009da6b44a2f8563f19c5e1f1b63c447f91d1
Babatunde Fatoye Sunday <babatunde.fatoye.sunday@framgia.com>
2016-03-10 16:47:11 +0700

    Fix Details of Feature 3

--- app/src/main/java/com/framgia/framgiarss/ui/activity/DetailedNews.java
+++ app/src/main/java/com/framgia/framgiarss/ui/activity/DetailedNews.java
@@ -4,15 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.framgia.framgiarss.R;
+import com.framgia.framgiarss.anim.AnimationUtils;
 import com.framgia.framgiarss.utils.Constants;
+import com.framgia.framgiarss.utils.ImageDownloaderTask;
 
 public class DetailedNews extends ActionBarActivity {
     private String title, url, image, des;
     private Toolbar mtoolbar;
     private TextView description;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +37,26 @@ public class DetailedNews extends ActionBarActivity {
         getSupportActionBar().setTitle(title);
 
         description = (TextView) findViewById(R.id.description);
+        imageView = (ImageView) findViewById(R.id.news_image);
         description.setText(des);
+        new ImageDownloaderTask(imageView).execute(image);
+        AnimationUtils.animateToolbarDroppingDown(imageView);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.details_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.goto_link) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
--- app/src/main/java/com/framgia/framgiarss/ui/adapter/NewsDataAdapter.java
+++ app/src/main/java/com/framgia/framgiarss/ui/adapter/NewsDataAdapter.java
@@ -1,15 +1,19 @@
 package com.framgia.framgiarss.ui.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.framgia.framgiarss.R;
 import com.framgia.framgiarss.data.RSSFeed;
+import com.framgia.framgiarss.ui.activity.DetailedNews;
 import com.framgia.framgiarss.utils.Constants;
+import com.framgia.framgiarss.utils.ImageDownloaderTask;
 
 import java.util.List;
 
@@ -36,6 +40,7 @@ public class NewsDataAdapter extends RecyclerView.Adapter<NewsDataAdapter.FeedLi
         feedListRowHolder.rssFeed = feedItem;
         feedListRowHolder.title.setText(feedItem.getTitle());
         feedListRowHolder.pubDate.setText(feedItem.getPublished_date());
+        new ImageDownloaderTask(feedListRowHolder.imageView).execute(feedItem.getEnclosure());
     }
 
     @Override
@@ -47,11 +52,24 @@ public class NewsDataAdapter extends RecyclerView.Adapter<NewsDataAdapter.FeedLi
         protected TextView title;
         protected TextView pubDate;
         protected RSSFeed rssFeed;
+        protected ImageView imageView;
 
         public FeedListRowHolder(View view) {
             super(view);
             this.title = (TextView) view.findViewById(R.id.tvtitle);
             this.pubDate = (TextView) view.findViewById(R.id.tvpubdate);
+            this.imageView = (ImageView)view.findViewById(R.id.news_images);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent data = new Intent(v.getContext(), DetailedNews.class);
+                    data.putExtra(Constants.ConstantKeys.TITILE, rssFeed.getTitle());
+                    data.putExtra(Constants.ConstantKeys.URL, rssFeed.getLink());
+                    data.putExtra(Constants.ConstantKeys.ENCLOSURE, rssFeed.getEnclosure());
+                    data.putExtra(Constants.ConstantKeys.DESCRIPTION, rssFeed.getDescription());
+                    v.getContext().startActivity(data);
+                }
+            });
         }
     }
 
